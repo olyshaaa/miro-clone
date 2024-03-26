adc2e13ada48c53b7631ae41bcae0fe1234c6271
olyshaaa <olgasevel108@gmail.com>
2024-03-26 20:47:08 +0200

    deployment

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,7 +6,7 @@ import { ConvexHttpClient } from 'convex/browser'
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 const liveblocks = new Liveblocks({
-	secret: 'sk_dev_IJyma0gzsSDKSM8XL7Hkhvp1nJq7PC9euYG292NGit94KoSFcKqclnAZ6RylwNte',
+	secret: process.env.LIVEBLOCKS_SECRET_KEY!
 })
 
 export async function POST(request: Request){
